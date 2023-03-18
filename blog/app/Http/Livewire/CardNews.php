3134b84bfdaff9a978ac53f9ed5c0f276f19fe42
Arthur Brasa <arthurbrasa@outlook.com>
2023-03-18 15:30:00 -0300

    Controllers/Models

---
+++ blog/app/Http/Livewire/CardNews.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CardNews extends Component
+{
+
+    
+    public function render()
+    {
+        return view('livewire.card-news');
+    }
+}
---
+++ blog/resources/views/livewire/card-news.blade.php
@@ -0,0 +1,12 @@
+{{-- CARDS --}}
+<div class="card">
+    <h5 class="card-header">{{ $title }}</h5>
+    <div class="card-body">
+      <h5 class="card-title"> {{ $SubTitle }}</h5>
+      <p class="card-text">
+        {{ $content }}
+      </p>
+      <a href="#" class="btn btn-primary">More</a>
+    </div>
+</div>
+{{-- FIM CARDS --}}
